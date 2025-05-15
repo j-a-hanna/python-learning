541189cad992ce0548cd69fea11296885342bf50
Jeanette Attia Hanna <jhanna2030@outlook.com>
2025-05-14 20:58:55 -0400

    Add age_check.py to practice conditionals

---
+++ exercises/age_check.py
@@ -0,0 +1,12 @@
+# age_check.py
+
+age = int(input("How old are you? "))
+
+if age < 13:
+    print("You are a child.")
+elif age < 20:
+    print("You are a teenager.")
+elif age < 65:
+    print("You are an adult.")
+else:
+    print("You are royalty.")
