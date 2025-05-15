86bbe3e064210e04bd1c216a8ca5756203f21242
Jeanette Attia Hanna <jhanna2030@outlook.com>
2025-05-14 21:23:41 -0400

    Update age_check.py to include voting age logic

--- exercises/age_check.py
+++ exercises/age_check.py
@@ -1,6 +1,14 @@
 # age_check.py
-
+#expanded version: voting and age group
 age = int(input("How old are you? "))
+if age < 13:
+    print("You're a child.")
+elif age < 20:
+    print("You're a teenager.")
+elif age < 65:
+    print("You're an adult.")
+else:
+    print("You're a senior.")
 
 if age < 18:
     print("You're not old enough to vote.")
