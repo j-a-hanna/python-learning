10136931434673e1de6d1acdd7502abb6ae69002
Jeanette Attia Hanna <jhanna2030@outlook.com>
2025-05-14 21:27:51 -0400

    add number_check.py to practice conditionals

---
+++ exercises/number_check.py
@@ -0,0 +1,10 @@
+# number_check.py
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("That's a positive number.")
+elif num < 0:
+    print("That's a negative number.")
+else:
+    print("That's a zero!")
